cf8f27de30193e4becbe7b7770a6195288064c55
vinhhoanglong <longvinhvhbk@gmail.com>
2024-10-21 19:57:39 +0700

    update conv2d size

--- model/CNN.py
+++ model/CNN.py
@@ -5,12 +5,12 @@ class CNN(nn.Module):
     def __init__(self, dropout):
         super(CNN, self).__init__()
         self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(32, 128, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
 
         self.pool = nn.MaxPool2d(2, 2)
 
-        self.fc1 = nn.Linear(128*4*4, 256)
+        self.fc1 = nn.Linear(256*4*4, 256)
         self.fc2 = nn.Linear(256, 10)
 
         self.dropout = nn.Dropout(dropout)
@@ -20,7 +20,7 @@ class CNN(nn.Module):
         x = self.pool(torch.relu(self.conv2(x)))
         x = self.pool(torch.relu(self.conv3(x)))
 
-        x = x.view(-1, 128*4*4)
+        x = x.view(-1, 256*4*4)
         x = torch.relu(self.fc1(x))
         x = self.dropout(x)
         x = self.fc2(x)
